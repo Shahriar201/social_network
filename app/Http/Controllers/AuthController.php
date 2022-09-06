7a9ca74a64d64f8f109ae5991dbe7a3b106fd50e
shahriar201 <shahriarbrine201@gmail.com>
2022-09-06 20:48:55 +0600

    response trait setup

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Traits\ApiResponser;
+use Illuminate\Support\Facades\Validator;
 
 class AuthController extends Controller
 {
+    use ApiResponser;
+
     public function register(Request $request) {
-        dd('Ok');
+        $validator = Validator::make($request->all(), [
+            'first_name'      => 'required',
+            'last_name'      => 'required',
+            'email'     => 'required|email|unique:persons,email',
+            'password'  => 'required|string|min:8',
+        ]);
+
+        if($validator->fails()) {
+            return $this->set_response(null, 422, 'failed', $validator->errors()->all());
+        }
     }
 }
