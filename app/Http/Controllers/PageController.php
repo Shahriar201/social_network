afb4d169b073781cbf6d073d2877ffc9d63123c0
shahriar201 <shahriarbrine201@gmail.com>
2022-09-07 00:48:21 +0600

    page created done

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -2,11 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Page;
+use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class PageController extends Controller
 {
+    use ApiResponser;
+
     public function createPage(Request $request) {
-        dd('Ok');
+        $validator = Validator::make($request->all(), [
+            'page_name'      => 'required',
+        ]);
+
+        if($validator->fails()) {
+            return $this->set_response(null, 422, 'failed', $validator->errors()->all());
+        }
+
+        $page = new Page();
+        $page->person_id = auth()->user()->id;
+        $page->page_name = $request->page_name;
+        $page->created_by = auth()->user()->id;
+        $page->save();
+
+        return $this->set_response($page, 200, 'success', ['Person Created Successfully']);
     }
 }
---
+++ database/migrations/2022_09_06_182051_create_pages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('person_id');
+            $table->string('page_name');
+            $table->tinyInteger('is_follow')->comment('1=>yes, 0=>no')->nullable();
+            $table->tinyInteger('created_by')->nullable();
+            $table->tinyInteger('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+}
