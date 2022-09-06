b12bc324104ba253703c388562ab8808c9d1ee9e
shahriar201 <shahriarbrine201@gmail.com>
2022-09-06 23:22:46 +0600

    user login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Person;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Traits\ApiResponser;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -24,13 +26,57 @@ class AuthController extends Controller
         }
 
         // create person
-        $user = Person::create([
+        $person = Person::create([
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
             'email' => $request->email,
             'password' =>  bcrypt($request->password),
         ]);
 
-        return $this->set_response($user, 200, 'success', ['Person Created Successfully']);
+        return ['person' => $person];
+
+        // return $this->set_response($person, 200, 'success', ['Person Created Successfully']);
+    }
+
+    public function login(Request $request) {
+
+        $validator = Validator::make($request->all(), [
+            'email'     => 'required|email',
+            'password'  => 'required'
+        ]);
+
+        if($validator->fails()) {
+            return $this->set_response(null, 422, 'failed', $validator->errors()->all());
+        }
+
+        $credentials =  $request->only('email', 'password');
+
+        if (!Auth::attempt($credentials)) {
+            return $this->set_response(null, 400, 'failed', ['Credentials does not match']);
+        }
+
+        if(Auth::attempt($credentials)) {
+            $user = $request->user();
+            $tokenRes = $user->createToken('Create Token');
+            $token = $tokenRes->token;
+            $token->expires_at = Carbon::now()->addWeeks(1);
+            $token->save();
+
+            $tokenData = [
+                'access_token'  => $tokenRes->accessToken,
+
+                'user' => [
+                    'token_type'    => 'Bearer',
+                    'expires_at'    => Carbon::parse($token->expires_at)->toDateTimeString(),
+                    'first_name'    => $user->first_name,
+                    'last_name'     => $user->last_name,
+                    'email'         => $user->email,
+                ]
+            ];
+            // return ['responseData' => $tokenData];
+            return $this->set_response($tokenData, 200, 'success', ['Logged in!']);
+        }
+
+
     }
 }
--- app/User.php
+++ app/User.php
@@ -16,6 +16,10 @@ class User extends Authenticatable
      *
      * @var array
      */
+    protected $table = 'people';
+
+    public $timestamps = false;
+
     protected $fillable = [
         'name', 'email', 'password',
     ];
