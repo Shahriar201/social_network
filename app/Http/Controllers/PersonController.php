7b9e06c5ab206c2b8ed3e79e5956b3c1dbb1bac7
shahriar201 <shahriarbrine201@gmail.com>
2022-09-07 23:05:15 +0600

    news feed view

--- app/Http/Controllers/PersonController.php
+++ app/Http/Controllers/PersonController.php
@@ -6,6 +6,7 @@ use App\Person;
 use App\Post;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 
 class PersonController extends Controller
@@ -14,12 +15,20 @@ class PersonController extends Controller
 
     public function personFeed(Request $request) {
         $userId = auth()->user()->id;
-        // dd($userId);
+
         $feed = Post::with('person', 'page', 'follow')
                 ->where('created_by', $userId)
                 ->get()->toArray();
 
         return $this->set_response($feed, 200, 'success', ['Logged in user news feed']);
+
+        // $newsFeed = DB::table('posts as post')
+        //             ->where('post.created_by', $userId)
+        //             ->leftJoin('people as people', 'people.id', 'post.created_by')
+        //             ->leftJoin('pages as page', 'page.id', 'post.page_id')
+        //             ->leftJoin('follows as follow', 'follow.created_by', 'post.created_by')
+        //             ->get()->toArray();
+        //             dd($newsFeed);
 
     }
 }
